9b0472c9a1f1fe9906c2998a5c4131a96d1e453d
NNallapu <nandana221@gmail.com>
2017-12-10 15:53:25 -0600

    Add files via upload
    Commit 2

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,6 +1,6 @@
 import random
 
-top = 100
+end = 100
 
 #Dictionary to store the Chutes to jump up(ladder) or fall down(snake)
 chutes = {1: 38, 4: 14, 9: 31, 16: 6, 21: 42, 28: 84, 36: 44,
@@ -60,27 +60,36 @@ def no_of_moves():
             i = i + d
 
     return n
+
+
 def game_path():
     """
 
     :return:
     Calculates and returns the minimum rolls taken to reach each square
     """
-    open = {0}
+    start = {0}
     path = {0: ()}
 
-    while len(open) > 0:
-        i = open.pop()
+    while len(start) > 0:
+        i = start.pop()
         p = path[i] + (i,)
         for j in range(i + 1, i + 7):
-            if j > top: break
+            if j > end: break
             if j in chutes: j = chutes[j]
             if j not in path or len(path[j]) > len(p):
-                open.add(j)
+                start.add(j)
                 path[j] = p
     for i in path:
-        a = print("Square", i, "can be reached in", len(path[i]), "rolls via", path[i])
-    return a
+        if(i == 100):
+            print("=====================================================================")
+            print("SQUARE 100 CAN BE REACHED IN ", len(path[i]), "ROLLS VIA", path[i])
+            print("=====================================================================")
+        else:
+            game_path = print("Square", i, "can be reached in ", len(path[i]), "rolls via", path[i])
+    return game_path
+
+
 def monte_carlo():
     games = 0
     total = 0
@@ -91,13 +100,35 @@ def monte_carlo():
         average = total / 1000000
     return str(average)
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 if __name__ == '__main__':
     display_instruction()
+    print("\n")
+    game_path()
+    print("\n")
     a = no_of_moves()
     c = monte_carlo()
-    print("Game length is " + str(a) + "moves")
-    game_path()
-    print("1000000 simulations of the game gives an average of" + str(c) + " moves")
+    print("===================================================")
+    print("Current Game length is " + str(a) + " moves \n")
+    print("===================================================")
+    print("1000000 simulations of the game gives an average of " + str(c) + " moves\n")
+    print("===================================================")
+    print("Running the Snakes and Ladders game for two players \n")
+    Player1 = no_of_moves()
+    Player2 = no_of_moves()
+    if Player1 < Player2:
+        print("Winner of the game is Player1 with " +str(Player1)+ " moves. While Player2 had "+str(Player2)+ " moves")
+    else:
+        print("Winner of the game is Player2 with " +str(Player2)+ " moves. While Player1 had "+str(Player1)+ " moves")
+
 
 
